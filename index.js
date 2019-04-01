650ad54750c8bc945e5453cbf167c9ab496975a0
ashen9 <247668045@qq.com>
2019-04-01 15:34:48 +0800

    to support webpack4

--- index.js
+++ index.js
@@ -9,31 +9,38 @@ function webpack_cdn2local_plugin(options) {
 
 webpack_cdn2local_plugin.prototype.apply = function(compiler) {
   let that = this;
-  compiler.plugin('emit', function(compilation,callback) {
-    let text = compilation.assets['index.html'].source()
-      $ = cheerio.load(text);
-    $('[cdn = JSZXtrue]').each((e,i) => {
-      let cdnUrl = ''
-      let localPath = ''
-      if($(i).prop("tagName") === 'SCRIPT') {
-        cdnUrl = $(i).attr('src')
-      }else if ($(i).prop("tagName") === 'LINK') {
-        cdnUrl = $(i).attr('href')
-      }
-      localPath = url.parse(cdnUrl).pathname.split('/').pop();
-      that.cdnUrlList.push({cdnUrl, localPath})
-      text = text.toString().replace(cdnUrl, '/cdn/' + localPath);
-    })
-    compilation.assets['index.html'] = toAsset(text);
-    Promise.all(that.cdnUrlList.map(function (cdnUrl) {
-        return download(cdnUrl.cdnUrl)
-      })).then(res => {
-        res.forEach((data,i) => {
-          compilation.assets[`cdn/${that.cdnUrlList[i].localPath}`] = toAsset(data);
-        })
-        callback()
-      })
-  });
+  var emit = function emit(compilation, callback) {
+	  let text = compilation.assets['index.html'].source()
+	  $ = cheerio.load(text);
+	  $('[cdn = JSZXtrue]').each((e,i) => {
+		  let cdnUrl = ''
+		  let localPath = ''
+		  if($(i).prop("tagName") === 'SCRIPT') {
+			  cdnUrl = $(i).attr('src')
+		  }else if ($(i).prop("tagName") === 'LINK') {
+			  cdnUrl = $(i).attr('href')
+		  }
+		  localPath = url.parse(cdnUrl).pathname.split('/').pop();
+		  that.cdnUrlList.push({cdnUrl, localPath})
+		  text = text.toString().replace(cdnUrl, '/cdn/' + localPath);
+	  })
+	  compilation.assets['index.html'] = toAsset(text);
+	  Promise.all(that.cdnUrlList.map(function (cdnUrl) {
+		  return download(cdnUrl.cdnUrl)
+	  })).then(res => {
+		  res.forEach((data,i) => {
+			  compilation.assets[`cdn/${that.cdnUrlList[i].localPath}`] = toAsset(data);
+		  })
+		  callback()
+	  })
+	};
+
+  if (compiler.hooks) {
+  	var plugin = { name: 'Cdn2LocalPlugin' };
+  	compiler.hooks.emit.tapAsync(plugin, emit);
+  } else {
+  	compiler.plugin('emit', emit);
+  }
 };
 
 function toAsset(resp) {
