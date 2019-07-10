b98de0d13049f1158e2f871b4e548e0baf8bacb2
ashen9 <247668045@qq.com>
2019-07-10 13:48:25 +0800

    对图标库进行压缩

--- index.js
+++ index.js
@@ -1,78 +1,86 @@
 var cheerio = require('cheerio');
 let url = require('url');
 let http = require('http');
-
-
-function webpack_cdn2local_plugin(options) {
+var uglifycss = require('uglifycss');
+function webpack_cdn2local_plugin (options) {
 	this.cdnUrlList = [];
 	this.prefixPath = options.prefixPath || '/';
 }
 
-webpack_cdn2local_plugin.prototype.apply = function(compiler) {
+webpack_cdn2local_plugin.prototype.apply = function (compiler) {
 	let that = this;
-	var emit = function emit(compilation, callback) {
-		let text = compilation.assets['index.html'].source()
-		$ = cheerio.load(text);
-		$('[cdn2local]').each((e,i) => {
-			let cdnUrl = ''
-			let localPath = ''
-			if($(i).prop("tagName") === 'SCRIPT') {
-				cdnUrl = $(i).attr('src')
-			}else if ($(i).prop("tagName") === 'LINK') {
-				cdnUrl = $(i).attr('href')
+	var emit = function emit (compilation, callback) {
+		let text = compilation.assets['index.html'].source();
+		let $ = cheerio.load(text);
+		$('[cdn2local]').each((e, i) => {
+			let cdnUrl = '';
+			let localPath = '';
+			if ($(i).prop('tagName') === 'SCRIPT') {
+				cdnUrl = $(i).attr('src');
+			} else if ($(i).prop('tagName') === 'LINK') {
+				cdnUrl = $(i).attr('href');
 			}
 			localPath = url.parse(cdnUrl).pathname.split('/').pop();
-			that.cdnUrlList.push({cdnUrl, localPath})
-			text = text.toString().replace(cdnUrl, `${that.prefixPath}cdn/`  + localPath);
-		})
+			that.cdnUrlList.push({cdnUrl, localPath});
+			text = text.toString().replace(cdnUrl, `${that.prefixPath}cdn/` + localPath);
+		});
 		console.log(`您的字库完整路径前缀是：${that.prefixPath}cdn/`);
 		compilation.assets['index.html'] = toAsset(text);
 		Promise.all(that.cdnUrlList.map(function (cdnUrl) {
-			return download(cdnUrl.cdnUrl)
+			return download(cdnUrl.cdnUrl);
 		})).then(res => {
-			res.forEach((data,i) => {
-				compilation.assets[`cdn/${that.cdnUrlList[i].localPath}`] = toAsset(data);
-			})
-			callback()
-		})
+			res.forEach((data, i) => {
+				compilation.assets[`cdn/${that.cdnUrlList[i].localPath}`] = toAsset(data, checkFileExt(that.cdnUrlList[i].localPath));
+			});
+			callback();
+		});
 	};
 
 	if (compiler.hooks) {
-		var plugin = { name: 'Cdn2LocalPlugin' };
+		var plugin = {name: 'Cdn2LocalPlugin'};
 		compiler.hooks.emit.tapAsync(plugin, emit);
 	} else {
 		compiler.plugin('emit', emit);
 	}
 };
 
-function toAsset(resp) {
+function toAsset (resp, fileExt) {
+	if (fileExt === 'css') {
+		resp = uglifycss.processString(resp, {expandVars: true});
+	}
 	return {
-		source: function() {
+		source: function () {
 			return resp;
 		},
-		size: function() {
+		size: function () {
 			return resp.length;
 		}
-	}
+	};
 }
 
-function download(url, compilation, cdnUrl) {
+function download (url, compilation, cdnUrl) {
 	return new Promise(function (resolve, reject) {
-		http.get('http:' + url, function(resp) {
+		http.get('http:' + url, function (resp) {
 			if (resp.statusCode === 200) {
 				let str = '';
 				resp.on('data', function (data) {
 					str += data.toString();
-				})
+				});
 				resp.on('end', function (data) {
-					resolve(str)
-				})
-			}
-			else {
+					resolve(str);
+				});
+			} else {
 				reject(new Error('[webpack-example-plugin] Unable to download the image'));
 			}
 		});
-	})
+	});
+}
+function checkFileExt (filename) {
+	// 取出上传文件的扩展名
+	var index = filename.lastIndexOf('.');
+	var ext = filename.substr(index + 1);
+	// 循环比较
+	return ext;
 }
 
 module.exports = webpack_cdn2local_plugin;
